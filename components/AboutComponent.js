e328e76be83a68ff7cc09c4158c7f011a0f8ad1b
Zhaohui Shan <szhandrew@gmail.com>
2020-09-16 02:11:41 -0700

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { Text, ScrollView, View, FlatList, SafeAreaView} from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -43,24 +44,43 @@ class About extends Component {
         };
 
 
-        return(
-            <SafeAreaView>
+        if (this.props.leaders.isLoading) {
+            return(
                 <ScrollView>
                     <History />
-                    
-                    <Card title="Corporate Leadership">
-                        <FlatList 
-                            data={this.props.leaders.leaders}
-                            renderItem={renderLeader}
-                            keyExtractor={item => item.id.toString()}
+                    <Card
+                        title='Corporate Leadership'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else if (this.props.leaders.errMess) {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                        <Text>{this.props.leaders.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                    <FlatList 
+                        data={this.props.leaders.leaders}
+                        renderItem={renderLeader}
+                        keyExtractor={item => item.id.toString()}
                         />
                     </Card>
                 </ScrollView>
-                
-                
-            </SafeAreaView>
-            
-        );
+            );
+        }
     }
 }
 
--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { View, FlatList } from 'react-native';
+import { View, FlatList, Text } from 'react-native';
 import { Tile } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
+
 
 const mapStateToProps = state => {
     return {
-      dishes: state.dishes
+        dishes: state.dishes,
     }
   }
 
@@ -18,7 +20,6 @@ class Menu extends Component {
     };
 
     render() {
-        const { navigate } = this.props.navigation;
 
         const renderMenuItem = ({item, index}) => {
             return (
@@ -33,13 +34,29 @@ class Menu extends Component {
             );
         };
 
-        return (
-            <FlatList 
-            data={this.props.dishes.dishes}
-            renderItem={renderMenuItem}
-            keyExtractor={item => item.id.toString()}
-            />
-        );
+        const { navigate } = this.props.navigation;
+
+        if (this.props.dishes.isLoading) {
+            return(
+                <Loading />
+            );
+        }
+        else if (this.props.dishes.errMess) {
+            return(
+                <View>            
+                    <Text>{this.props.dishes.errMess}</Text>
+                </View>            
+            );
+        }
+        else {
+            return (
+                <FlatList 
+                    data={this.props.dishes.dishes}
+                    renderItem={renderMenuItem}
+                    keyExtractor={item => item.id.toString()}
+                    />
+            );
+        }
     }
 }
 
